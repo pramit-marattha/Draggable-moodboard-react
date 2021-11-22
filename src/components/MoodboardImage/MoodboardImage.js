78c8d9c8fa60d342dfa741c1c8adaeda47a7f24f
pramit-marattha <pramitmarattha5497@gmail.com>
2021-11-22 08:38:18 +0545

    Moodboard and MoodboardImage component updated

--- src/components/MoodboardImage/MoodboardImage.js
+++ src/components/MoodboardImage/MoodboardImage.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
 
-const MoodboardImage = () => {
+const type = "Image"; // Need to pass which type element can be draggable
+
+const Image = ({ image, index, moveImage }) => {
+  const ref = useRef(null);
+
+  const [, drop] = useDrop({
+    accept: type,
+    hover(item) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      moveImage(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    }
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    item: { type, id: image.id, index },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+
+  drag(drop(ref));
+
+  return (
+    <div
+      ref={ref}
+      style={{ opacity: isDragging ? 0 : 1 }}
+      className="moodboard-img-item"
+    >
+      <img alt={`img - ${image.id}`} src={image.src} className="moodboard-img" />
+    </div>
+  );
+};
+
+const MoodboardImage = ({ images, moveImage }) => {
+  const renderImage = (image, index) => {
     return (
-        <div>
-            
-        </div>
-    )
-}
+      <Image
+        image={image}
+        index={index}
+        key={`${image.id}-image`}
+        moveImage={moveImage}
+      />
+    );
+  };
 
-export default MoodboardImage
+  return <section className="moodboard-img-list">{images.map(renderImage)}</section>;
+};
+
+export default MoodboardImage;
